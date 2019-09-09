f52e225102f93fe2cff9f5dbd82a03707579389b
Casanova <raphael.casanova@wagner-group.com>
2019-09-09 19:38:27 +0200

    add state, itemlist and button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // Import MaterialApp and other widgets which we can use to quickly create a material app
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 // Code written in Dart starts exectuting from the main function. runApp is part of
 // Flutter, and requires the component which will be our app's container. In Flutter,
@@ -10,14 +11,56 @@ void main() => runApp(new TodoApp());
 class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Todo List',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Todo List')
-        )
-      ),
-    );
+    return new MaterialApp(title: 'Todo List', home: new TodoList());
   }
 }
 
+class TodoList extends StatefulWidget {
+  @override
+  createState() => new TodoListState();
+}
+
+class TodoListState extends State<TodoList> {
+  List<String> _todoItems = [];
+
+// This will be called each time the + button is pressed
+  void _addTodoItem() {
+    // Putting our code inside "setState" tells the app that our state has changed, and
+    // it will automatically re-render the list
+    setState(() {
+      int index = _todoItems.length;
+      _todoItems.add('Item ' + index.toString());
+    });
+  }
+
+  // Build the whole list of todo items
+  Widget _buildTodoList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        // itemBuilder will be automatically be called as many times as it takes for the
+        // list to fill up its available space, which is most likely more than the
+        // number of todo items we have. So, we need to check the index is OK.
+        if (index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index]);
+        }
+      },
+    );
+  }
+
+  // Build a single todo item
+  Widget _buildTodoItem(String todoText) {
+    return new ListTile(title: new Text(todoText));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Todo List')),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: _addTodoItem,
+          tooltip: 'Add task',
+          child: new Icon(Icons.add)),
+    );
+  }
+}
